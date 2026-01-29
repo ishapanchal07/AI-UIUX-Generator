599b43cf15379772ff0b9388c868150c762b3408
ishapanchal07 <isha7112003@gmail.com>
2026-01-29 21:23:21 +0530

    initial Commit (Project Setup)

---
+++ uiuxmock/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const appFont=DM_Sans({subsets:['latin']})
+export const metadata: Metadata = {
+  title: "UIUX Mockup generator App",
+  description: "Generate High quality free UIUX Mobile and Web Mockup desings",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="appFont.className">
+        {children}
+      </body>
+    </html>
+  );
+}
