d64127b71967c688016b0141daf62b725f159425
ishapanchal07 <isha7112003@gmail.com>
2026-01-30 12:25:46 +0530

    Authentication using clerk

--- uiuxmock/app/layout.tsx
+++ uiuxmock/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
+import { Provider } from "@radix-ui/react-tooltip";
 
 const appFont=DM_Sans({subsets:['latin']})
 export const metadata: Metadata = {
@@ -18,7 +19,9 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className="appFont.className">
+        <Provider>
         {children}
+        </Provider>
       </body>
     </html>
     </ClerkProvider>
