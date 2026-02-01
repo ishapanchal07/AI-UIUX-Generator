a254667cdc17794637d3590df796ec05f78b2b21
ishapanchal07 <isha7112003@gmail.com>
2026-02-01 11:11:05 +0530

    header

--- uiuxmock/app/_shared/Header.tsx
+++ uiuxmock/app/_shared/Header.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/assets/logo.png";
 import { Button } from "@/components/ui/button";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 
 function Header() {
+  const { user } = useUser();
+
   return (
     <div className="flex items-center justify-between p-4">
       <div className="flex gap-2 items-center">
         <Image src={logo} alt="logo" width={40} height={40} />
-        <h2 className="text-xl font-semibold"><span className="text-primary">UIUX</span> MOCK</h2>
+        <h2 className="text-xl font-semibold">
+          <span className="text-primary">UIUX</span> MOCK
+        </h2>
       </div>
 
       <ul className="flex gap-5 items-center text-lg">
@@ -15,7 +22,14 @@ function Header() {
         <li className="hover:text-primary cursor-pointer">Pricing</li>
       </ul>
 
-      <Button>Get Started</Button>
+      {!user ? (
+        <SignInButton mode='modal'>
+          <Button>Get Started</Button>
+          </SignInButton>
+
+      ) : (
+        <UserButton />
+      )}
     </div>
   );
 }
--- uiuxmock/app/layout.tsx
+++ uiuxmock/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from '@clerk/nextjs'
-import { Provider } from "@radix-ui/react-tooltip";
+import { ClerkProvider } from "@clerk/nextjs";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
-const appFont=DM_Sans({subsets:['latin']})
+const appFont = DM_Sans({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title: "UIUX Mockup generator App",
   description: "Generate High quality free UIUX Mobile and Web Mockup desings",
@@ -12,18 +13,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider>
     <html lang="en">
-      <body className="appFont.className">
-        <Provider>
-        {children}
-        </Provider>
+      <body className={appFont.className}>
+        <ClerkProvider>
+          <TooltipProvider>
+            {children}
+          </TooltipProvider>
+        </ClerkProvider>
       </body>
     </html>
-    </ClerkProvider>
   );
 }
